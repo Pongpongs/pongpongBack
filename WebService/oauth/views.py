e7ad1696be7a1fa9c0cd55c41cf893f01bbc7c14
magnificentLee <leer9999@gmail.com>
2024-02-27 13:10:16 +0900

    latest

--- WebService/oauth/views.py
+++ WebService/oauth/views.py
@@ -5,6 +5,9 @@ import requests
 from urllib.parse import urlencode
 import os
 from django.contrib import messages
+from django.core.cache import cache
+import redis
+import json
 
 # Create your views here.
 def oauth_login(request):
@@ -20,6 +23,9 @@ def oauth_login(request):
     }
 	auth_url = f"https://api.intra.42.fr/oauth/authorize?{urlencode(params)}"
 	return redirect(auth_url)
+
+def get_authorization_code(request):
+	return request.GET.get('code')
 
 def get_access_token(code):
 	client_id = os.getenv('CLIENT_ID')
@@ -52,12 +58,12 @@ def oauth_callback(request):
 	# GET 요청만 처리, 예기치 않은 요청 메소드는 거부
 	if request.method != 'GET':
 		return HttpResponseNotAllowed(['GET'])
-	access_code = request.GET.get('code')
-	if not access_code:
+	authorization_code = get_authorization_code(request)
+	if not authorization_code:
 		# 접근 코드 발급 실패시 보일 화면
 		return redirect('/access_code_erorr/')
 	# 2FA 인증 위치
-	access_token, status_code = get_access_token(access_code)
+	access_token, status_code = get_access_token(authorization_code)
 	if not access_token:
 		# 토큰 발급 실패시 보일 화면
 		error_message = f"Login failed with error code: {status_code}"
@@ -70,6 +76,8 @@ def oauth_callback(request):
 	registerUserinDB(user_info)
 	# print_all_users() # db에 등록된 모든 유저 출력
 	# 로그인 성공시 보일 첫 화면
+	access_token_store_and_fetch(access_token)
+	get_token_info_from_redis(access_token)
 	return redirect('/index/')
 
 def registerUserinDB(user_info):
@@ -116,6 +124,51 @@ def get_token_info(access_token):
 	response = requests.get(token_info_endpoint, headers=headers)
 	if response.status_code == 200:
 		token_info = response.json()
-		return print(token_info)
-	else:
-		return print(f"Invalid Token. Status code: {response.status_code}, Response: {response.text}")
+		return token_info, None
+	status_code = response.status_code
+	return None, status_code
+
+# redis test
+def store_access_token(access_token):
+	# store the access_token in the cache in 10 seconds
+	cache.set('access_token', access_token, timeout=10)
+
+def fetch_access_token():
+	# fetch the access_token from the cache
+	return cache.get('access_token')
+
+def access_token_store_and_fetch(access_token):
+	token_info = get_token_info(access_token)
+	# token_info = ({key: value}, {key, value} ...) tuple
+	# token_info[]
+	json_data = json.dumps(token_info[0])
+    # Connect to Redis
+    # Adjust the connection parameters as necessary
+	r = redis.Redis(host='localhost', port=6379, db=0)
+
+    # Use the access_token as the key and the serialized JSON as the value
+    # You might want to use a more specific key based on your application's needs
+	r.set(access_token, json_data)
+
+	print(f"Stored token info for access token: {access_token}")
+
+def get_token_info_from_redis(access_token):
+    # Connect to Redis
+	r = redis.Redis(host='localhost', port=6379, db=0)
+
+    # Retrieve the serialized JSON data by access_token
+	json_data = r.get(access_token)
+	
+	if json_data:
+		# Deserialize the JSON string back into a Python dictionary
+		print(f"type of redis : {type(r)}") # redis.client.Redis
+		print(f"type of token : {type(access_token)}") # str
+		print(f"type of get(token)  : {type(json_data)}") # bytes
+		data = json.loads(json_data)
+		print(f"type of json : {type(data)}") # dict
+		print("Data found")
+		for key in data:
+			print(f"{key} : {data[key]}")
+		return data
+	print(f"No data found for access token: {access_token}")
+	return None
