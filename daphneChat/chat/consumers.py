98823341d6cf9d10740758ce0487afc3cf2c0626
magnificentLee <leer9999@gmail.com>
2024-03-13 14:29:17 +0900

    add channel layer

---
+++ daphneChat/chat/consumers.py
@@ -0,0 +1,72 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+	async def connect(self):
+		# group을 미리 만들거나 특정 닉네임을 기준으로 만드는방식?
+		# 그룹에 빈공간이 있는지, 특정 닉네님이 있는지 확인
+		self.room_name = self.scope['url_route']['kwargs']['room_name']
+		self.room_group_name = "chat_" + self.room_name
+		await (self.channel_layer.group_add(
+			self.room_group_name,
+			self.channel_name
+		))
+		await self.accept()
+	
+	async def disconnect(self, close_code):
+		await (self.channel_layer.group_discard(
+			self.room_group_name,
+			self.channel_name
+		))
+	
+	async def receive(self, text_data):
+		text_data_json = json.loads(text_data)
+		message = text_data_json["message"]
+
+		# 계산식
+		await (self.channel_layer.group_send(
+			self.room_group_name,
+			{
+				"type": "chat.message",
+				"message": message
+			}
+		))
+	async def chat_message(self, event):
+		message = event["message"]
+		await (self.send(text_data=json.dumps({
+			"message": message
+		})))
+
+# import json
+# from channels.generic.websocket import WebsocketConsumer
+# from asgiref.sync import async_to_sync
+
+# class ChatConsumer(WebsocketConsumer):
+# 	def connect(self):
+# 		self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+# 		self.room_group_name = "chat_" + self.room_name
+# 		async_to_sync(self.channel_layer.group_add)(
+# 			self.room_group_name, self.channel_name
+# 		)
+# 		self.accept()
+	
+# 	def disconnect(self, close_code):
+# 		async_to_sync(self.channel_layer.group_discard)(
+# 			self.room_group_name, self.channel_name
+# 		)
+	
+# 	def receive(self, text_data):
+# 		text_data_json = json.loads(text_data)
+# 		message = text_data_json["message"]
+
+# 		async_to_sync(self.channel_layer.group_send)(
+# 			self.room_group_name, {
+# 				"type": "chat.message",
+# 				"message": message
+# 			}
+# 		)
+# 	def chat_message(self, event):
+# 		message = event["message"]
+# 		self.send(text_data=json.dumps({
+# 			"message": message
+# 		}))
