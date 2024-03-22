80cedb980c4f85146bb3c89595a03844d3b48f45
Byoung-Wha Cho <whqudghk28@gmail.com>
2024-03-22 12:41:29 +0900

    feat: new local offline

--- daphneChat_local_v2/chat/consumers.py
+++ daphneChat_local_v2/chat/consumers.py
@@ -7,26 +7,31 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class GameManager:
     def __init__(self):
         self.games = {}
+        self.room_sessions = {}
 
-    def get_or_create_game(self, room_name):
-        if room_name not in self.games:
-            self.games[room_name] = {
-                'play_bar1_position': {'x': 0, 'y': 9},
-                'play_bar2_position': {'x': 0, 'y': -9},
-                'ball_position': {'x': 0, 'y': 0},
-                'ball_velocity': {'x': 0.03, 'y': 0.02},
-                'score_player1': 0,
-                'score_player2': 0,
-                'game_over_flag': False,
-                'game_winner': 0,
-                'updating_ball_position': False,
-                'connected_clients_count': 0
-            }
-        return self.games[room_name]
+    def create_new_game_session(self, room_name):
+        session_id = str(uuid.uuid4())
+        self.room_sessions[room_name] = session_id
+        self.games[session_id] = {
+            'play_bar1_position': {'x': 0, 'y': 9},
+            'play_bar2_position': {'x': 0, 'y': -9},
+            'ball_position': {'x': 0, 'y': 0},
+            'ball_velocity': {'x': 0.03, 'y': 0.02},
+            'score_player1': 0,
+            'score_player2': 0,
+            'game_over_flag': False,
+            'game_winner': 0,
+            'updating_ball_position': False,
+            'connected_clients_count': 0
+        }
+        return session_id
 
-    def end_game(self, room_name):
-        if room_name in self.games:
-            del self.games[room_name]
+    def get_game_state(self, session_id):
+        return self.games.get(session_id)
+
+    def end_game_session(self, session_id):
+        if session_id in self.games:
+            del self.games[session_id]
 
 
 game_manager = GameManager()
@@ -35,25 +40,20 @@ game_manager = GameManager()
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-
-        # group을 미리 만들거나 특정 닉네임을 기준으로 만드는방식?
-        # 그룹에 빈공간이 있는지, 특정 닉네님이 있는지 확인
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = "chat_" + self.room_name
+
+        self.session_id = game_manager.create_new_game_session(self.room_name)
+        self.game_state = game_manager.get_game_state(self.session_id)
 
         await (self.channel_layer.group_add(
-            self.room_group_name,
+            self.session_id,  # 세션 ID를 사용하여 그룹 추가
             self.channel_name
         ))
 
         await self.accept()
+        self.game_state['connected_clients_count'] += 1
 
-        self.game_state = game_manager.get_or_create_game(self.room_name)
-        self.game_state['connected_clients_count'] += 1  # 클라이언트 수 증가
-
-        self.player_number = self.game_state['connected_clients_count']
-
-        if self.game_state['connected_clients_count'] == 2:
+        if self.game_state['connected_clients_count'] == 1:
             await asyncio.sleep(3)  # 클라이언트가 2개 연결된 후 3초 기다립니다.
             if not self.game_state['updating_ball_position']:
                 self.game_state['updating_ball_position'] = True
@@ -62,32 +62,26 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         self.game_state['connected_clients_count'] -= 1
         if self.game_state['connected_clients_count'] == 0:
-            game_manager.end_game(self.room_name)
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+            game_manager.end_game_session(self.session_id)
+        await self.channel_layer.group_discard(self.session_id, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        print(message, " ", self.channel_name)
+        # print(message, " ", self.channel_name)
 
-        if self.player_number == 1:
-            if message == 'a':
-                self.game_state['play_bar1_position']['x'] = max(
-                    -9, self.game_state['play_bar1_position']['x'] - 0.4)
-            elif message == 'd':
-                self.game_state['play_bar1_position']['x'] = min(
-                    9, self.game_state['play_bar1_position']['x'] + 0.4)
-
-        elif self.player_number == 2:
-            if message == 'a':
-                self.game_state['play_bar2_position']['x'] = max(
-                    -9, self.game_state['play_bar2_position']['x'] - 0.4)
-            elif message == 'd':
-                self.game_state['play_bar2_position']['x'] = min(
-                    9, self.game_state['play_bar2_position']['x'] + 0.4)
-
-        # pass
-        # self._update_ball_position()
+        if message == 'a':
+            self.game_state['play_bar1_position']['x'] = max(
+                -9, self.game_state['play_bar1_position']['x'] - 0.4)
+        elif message == 'd':
+            self.game_state['play_bar1_position']['x'] = min(
+                9, self.game_state['play_bar1_position']['x'] + 0.4)
+        elif message == 'j':
+            self.game_state['play_bar2_position']['x'] = max(
+                -9, self.game_state['play_bar2_position']['x'] - 0.4)
+        elif message == 'l':
+            self.game_state['play_bar2_position']['x'] = min(
+                9, self.game_state['play_bar2_position']['x'] + 0.4)
 
     async def _update_ball_position(self):
         # 공 위치 업데이트
@@ -121,17 +115,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.game_state['ball_velocity']['x'] *= -1  # x 방향 반전
 
         # 게임 종료 조건 확인
-        if self.game_state['score_player1'] >= self.game_state.get('game_over_score', 5) or self.game_state['score_player2'] >= self.game_state.get('game_over_score', 5):
+        if self.game_state['score_player1'] >= self.game_state.get('game_over_score', 3) or self.game_state['score_player2'] >= self.game_state.get('game_over_score', 3):
             self.game_state['game_over_flag'] = True
             self.game_state['game_winner'] = 1 if self.game_state['score_player1'] >= self.game_state.get(
-                'game_over_score', 5) else 2
+                'game_over_score', 3) else 2
 
-        # 게임 상태 업데이트를 모든 클라이언트에 전송
         await self.channel_layer.group_send(
-            self.room_group_name,
+            self.session_id,  # 세션 ID를 기반으로 그룹명 지정
             {
                 'type': 'game_update',
-                'game_state': self.game_state
+                # 'game_state': self.game_state
+                'play_bar1_position': self.game_state['play_bar1_position'],
+                'play_bar2_position': self.game_state['play_bar2_position'],
+                'ball_position': self.game_state['ball_position'],
+                'score_player1': self.game_state['score_player1'],
+                'score_player2': self.game_state['score_player2'],
+                'game_over_flag': self.game_state['game_over_flag'],
+                'game_winner': self.game_state['game_winner']
             }
         )
 
@@ -154,13 +154,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         game_manager.end_game(self.room_name)
 
     async def game_update(self, event):
-        # 이 메서드는 group_send 호출로 인해 자동으로 실행됩니다.
         await self.send(text_data=json.dumps({
-            'play_bar1_position': self.game_state['play_bar1_position'],
-            'play_bar2_position': self.game_state['play_bar2_position'],
-            'ball_position': self.game_state['ball_position'],
-            'score_player1': self.game_state['score_player1'],
-            'score_player2': self.game_state['score_player2'],
-            'game_over_flag': self.game_state['game_over_flag'],
-            'game_winner': self.game_state['game_winner']
+            'play_bar1_position': event['play_bar1_position'],
+            'play_bar2_position': event['play_bar2_position'],
+            'ball_position': event['ball_position'],
+            'score_player1': event['score_player1'],
+            'score_player2': event['score_player2'],
+            'game_over_flag': event['game_over_flag'],
+            'game_winner': event['game_winner']
         }))
